1dd01e9e9e19ff09d6ad2480de68e27a8c19eb69
jmateus <jmateus@jmateuss-iMac.local>
2020-06-12 16:28:49 +0100

    update JS JS

--- api/helpers/twitter.js
+++ api/helpers/twitter.js
@@ -1,13 +1,14 @@
 const URL = "https://api.twitter.com/1.1/search/tweets.json";
 const axios = require('axios');
-require('dotenv').config();
+require('dotenv').config(); //this module , help save secret words on a file 
 
 class Twitter {
     get(query, count) {
         return axios.get(URL, {
             params: {
                 q: query,
-                count: count
+                count: count,
+                tweet_mode: "extended",
             },
             headers: {
                 "Authorization" : `Bearer ${process.env.TWITTER_API_TOKEN}`, 
